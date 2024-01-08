7e4e3d8de41a697a83b530fc3f0455188550c5cc
Carmen Caniglia <carmencaniglia@gmail.com>
2024-01-08 13:11:35 +0100

    Initial commit

---
+++ src/main/java/carmen/bes5l1/BeS5L1Application.java
@@ -0,0 +1,13 @@
+package carmen.bes5l1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeS5L1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeS5L1Application.class, args);
+	}
+
+}
---
+++ src/test/java/carmen/bes5l1/BeS5L1ApplicationTests.java
@@ -0,0 +1,13 @@
+package carmen.bes5l1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeS5L1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
